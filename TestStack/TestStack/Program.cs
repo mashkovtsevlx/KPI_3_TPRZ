87564179867a35731b1db70a36a211b938d1487a
Alexander Mashkovtsev <mashkovtsev.lx@gmail.com>
2017-09-19 23:08:16 +0300

    New Program. TestStack

---
+++ TestStack/TestStack/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TestStack
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			string quit;
+			do
+			{
+				Console.Clear();
+				Stack<string> stack = new Stack<string>();
+				string addmore;
+				do
+				{
+					Console.WriteLine("Enter a value: ");
+					stack.Push(Console.ReadLine());
+					Console.WriteLine("Add more? (Any key/n)");
+					addmore = Console.ReadLine();
+				} while (addmore != "n");
+				Console.ForegroundColor = ConsoleColor.DarkGreen;
+				Console.WriteLine("Done.");
+				Console.ResetColor();
+				Console.ForegroundColor = ConsoleColor.DarkCyan;
+				Console.Write("Your Stack:");
+				while (true)
+				{
+					try
+					{
+						Console.Write(" {0}", stack.Pop());
+					}
+					catch (System.InvalidOperationException e)
+					{
+						break;
+					}
+				}
+				Console.ResetColor();
+				Console.WriteLine("\nNew Stack? (Any key/n)");
+				quit = Console.ReadLine();
+			} while (quit != "n");
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
