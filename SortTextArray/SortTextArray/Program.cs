b12f1bd1fb9fb0ff9af1639d8b959a77ed763d19
Alexander Mashkovtsev <mashkovtsev.lx@gmail.com>
2017-09-19 14:51:54 +0300

    New program. SortTextArray

---
+++ SortTextArray/SortTextArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SortTextArray
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int i;
+            string quit;
+            do
+            {
+                i = 0;
+                string[] array = new string[3];
+                Console.WriteLine("Enter first string: ");
+                array[0] = Console.ReadLine();
+                Console.WriteLine("Enter second string: ");
+                array[1] = Console.ReadLine();
+                Console.WriteLine("Enter third string: ");
+                array[2] = Console.ReadLine();
+                Array.Sort(array);
+                Console.WriteLine("\nSorted array: ");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                while (i < 3)
+                {
+                    Console.WriteLine(array[i]);
+                    i++;
+                }
+                Console.ResetColor();
+                Console.WriteLine("Continue? (y/n)");
+                quit = Console.ReadLine();
+            } while (quit == "y");
+        }
+    }
+}
