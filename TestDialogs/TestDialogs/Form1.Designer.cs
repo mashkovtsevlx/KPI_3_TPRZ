e8113cce10cb964e324bb1bcd0ec9df3ede1e6e5
Alexander Mashkovtsev <mashkovtsev.lx@gmail.com>
2017-09-21 20:26:23 +0300

    Finnalize program TestDialogs.

--- TestDialogs/TestDialogs/Form1.Designer.cs
+++ TestDialogs/TestDialogs/Form1.Designer.cs
@@ -28,13 +28,22 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.перевестиСтрокуНаАнглийскийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.создатьToolStripButton = new System.Windows.Forms.ToolStripButton();
 			this.открытьToolStripButton = new System.Windows.Forms.ToolStripButton();
 			this.сохранитьToolStripButton = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
 			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
 			this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
 			this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -43,9 +52,12 @@
 			this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-			this.создатьToolStripButton = new System.Windows.Forms.ToolStripButton();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.contextMenuStrip1.SuspendLayout();
 			this.toolStrip1.SuspendLayout();
 			this.menuStrip1.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// richTextBox1
@@ -53,11 +65,26 @@
 			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox1.ContextMenuStrip = this.contextMenuStrip1;
 			this.richTextBox1.Location = new System.Drawing.Point(12, 50);
 			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(378, 258);
+			this.richTextBox1.Size = new System.Drawing.Size(378, 245);
 			this.richTextBox1.TabIndex = 0;
 			this.richTextBox1.Text = "";
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.перевестиСтрокуНаАнглийскийToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(257, 26);
+			// 
+			// перевестиСтрокуНаАнглийскийToolStripMenuItem
+			// 
+			this.перевестиСтрокуНаАнглийскийToolStripMenuItem.Name = "перевестиСтрокуНаАнглийскийToolStripMenuItem";
+			this.перевестиСтрокуНаАнглийскийToolStripMenuItem.Size = new System.Drawing.Size(256, 22);
+			this.перевестиСтрокуНаАнглийскийToolStripMenuItem.Text = "Перевести строку на английский";
+			this.перевестиСтрокуНаАнглийскийToolStripMenuItem.Click += new System.EventHandler(this.перевестиСтрокуНаАнглийскийToolStripMenuItem_Click);
 			// 
 			// toolStrip1
 			// 
@@ -65,13 +92,29 @@
             this.создатьToolStripButton,
             this.открытьToolStripButton,
             this.сохранитьToolStripButton,
+            this.toolStripSeparator1,
             this.toolStripButton1,
-            this.toolStripButton2});
+            this.toolStripButton2,
+            this.toolStripSeparator,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5});
 			this.toolStrip1.Location = new System.Drawing.Point(0, 24);
 			this.toolStrip1.Name = "toolStrip1";
 			this.toolStrip1.Size = new System.Drawing.Size(402, 25);
 			this.toolStrip1.TabIndex = 1;
 			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// создатьToolStripButton
+			// 
+			this.создатьToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.создатьToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("создатьToolStripButton.Image")));
+			this.создатьToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.создатьToolStripButton.Name = "создатьToolStripButton";
+			this.создатьToolStripButton.Size = new System.Drawing.Size(23, 22);
+			this.создатьToolStripButton.Text = "&Создать";
+			this.создатьToolStripButton.Click += new System.EventHandler(this.создатьToolStripButton_Click);
+			this.создатьToolStripButton.MouseEnter += new System.EventHandler(this.создатьToolStripButton_MouseEnter);
 			// 
 			// открытьToolStripButton
 			// 
@@ -82,6 +125,7 @@
 			this.открытьToolStripButton.Size = new System.Drawing.Size(23, 22);
 			this.открытьToolStripButton.Text = "&Открыть";
 			this.открытьToolStripButton.Click += new System.EventHandler(this.открытьToolStripButton_Click);
+			this.открытьToolStripButton.MouseEnter += new System.EventHandler(this.открытьToolStripButton_MouseEnter_1);
 			// 
 			// сохранитьToolStripButton
 			// 
@@ -92,6 +136,12 @@
 			this.сохранитьToolStripButton.Size = new System.Drawing.Size(23, 22);
 			this.сохранитьToolStripButton.Text = "&Сохранить";
 			this.сохранитьToolStripButton.Click += new System.EventHandler(this.сохранитьToolStripButton_Click);
+			this.сохранитьToolStripButton.MouseEnter += new System.EventHandler(this.сохранитьToolStripButton_MouseEnter_1);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
 			// 
 			// toolStripButton1
 			// 
@@ -100,8 +150,8 @@
 			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.toolStripButton1.Name = "toolStripButton1";
 			this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-			this.toolStripButton1.Text = "toolStripButton1";
 			this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+			this.toolStripButton1.MouseEnter += new System.EventHandler(this.toolStripButton1_MouseEnter);
 			// 
 			// toolStripButton2
 			// 
@@ -110,8 +160,43 @@
 			this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.toolStripButton2.Name = "toolStripButton2";
 			this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
-			this.toolStripButton2.Text = "toolStripButton2";
 			this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+			this.toolStripButton2.MouseEnter += new System.EventHandler(this.toolStripButton2_MouseEnter);
+			// 
+			// toolStripSeparator
+			// 
+			this.toolStripSeparator.Name = "toolStripSeparator";
+			this.toolStripSeparator.Size = new System.Drawing.Size(6, 25);
+			// 
+			// toolStripButton3
+			// 
+			this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+			this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton3.Name = "toolStripButton3";
+			this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+			this.toolStripButton3.MouseEnter += new System.EventHandler(this.toolStripButton3_MouseEnter);
+			// 
+			// toolStripButton4
+			// 
+			this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+			this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton4.Name = "toolStripButton4";
+			this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+			this.toolStripButton4.MouseEnter += new System.EventHandler(this.toolStripButton4_MouseEnter);
+			// 
+			// toolStripButton5
+			// 
+			this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+			this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton5.Name = "toolStripButton5";
+			this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+			this.toolStripButton5.MouseEnter += new System.EventHandler(this.toolStripButton5_MouseEnter);
 			// 
 			// menuStrip1
 			// 
@@ -144,6 +229,7 @@
 			this.создатьToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
 			this.создатьToolStripMenuItem.Text = "&Создать";
 			this.создатьToolStripMenuItem.Click += new System.EventHandler(this.создатьToolStripMenuItem_Click);
+			this.создатьToolStripMenuItem.MouseEnter += new System.EventHandler(this.создатьToolStripMenuItem_MouseEnter);
 			// 
 			// открытьToolStripMenuItem
 			// 
@@ -154,6 +240,7 @@
 			this.открытьToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
 			this.открытьToolStripMenuItem.Text = "&Открыть";
 			this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+			this.открытьToolStripMenuItem.MouseEnter += new System.EventHandler(this.открытьToolStripMenuItem_MouseEnter_1);
 			// 
 			// toolStripSeparator2
 			// 
@@ -169,6 +256,7 @@
 			this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
 			this.сохранитьToolStripMenuItem.Text = "&Сохранить";
 			this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+			this.сохранитьToolStripMenuItem.MouseEnter += new System.EventHandler(this.сохранитьToolStripMenuItem_MouseEnter);
 			// 
 			// выходToolStripMenuItem
 			// 
@@ -176,6 +264,7 @@
 			this.выходToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
 			this.выходToolStripMenuItem.Text = "Вы&ход";
 			this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+			this.выходToolStripMenuItem.MouseEnter += new System.EventHandler(this.выходToolStripMenuItem_MouseEnter);
 			// 
 			// openFileDialog1
 			// 
@@ -183,31 +272,40 @@
 			this.openFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files(*.*)|*.*";
 			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
 			// 
-			// создатьToolStripButton
+			// statusStrip1
 			// 
-			this.создатьToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.создатьToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("создатьToolStripButton.Image")));
-			this.создатьToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.создатьToolStripButton.Name = "создатьToolStripButton";
-			this.создатьToolStripButton.Size = new System.Drawing.Size(23, 22);
-			this.создатьToolStripButton.Text = "&Создать";
-			this.создатьToolStripButton.Click += new System.EventHandler(this.создатьToolStripButton_Click);
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 298);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(402, 22);
+			this.statusStrip1.TabIndex = 3;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(402, 320);
+			this.Controls.Add(this.statusStrip1);
 			this.Controls.Add(this.toolStrip1);
 			this.Controls.Add(this.menuStrip1);
 			this.Controls.Add(this.richTextBox1);
 			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "KPI Pad";
+			this.contextMenuStrip1.ResumeLayout(false);
 			this.toolStrip1.ResumeLayout(false);
 			this.toolStrip1.PerformLayout();
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -230,6 +328,15 @@
 		private System.Windows.Forms.ToolStripButton toolStripButton2;
 		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
 		private System.Windows.Forms.ToolStripButton создатьToolStripButton;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripButton toolStripButton3;
+		private System.Windows.Forms.ToolStripButton toolStripButton4;
+		private System.Windows.Forms.ToolStripButton toolStripButton5;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem перевестиСтрокуНаАнглийскийToolStripMenuItem;
 	}
 }
 
--- TestDialogs/TestDialogs/Form1.cs
+++ TestDialogs/TestDialogs/Form1.cs
@@ -13,6 +13,9 @@ namespace TestDialogs
 {
 	public partial class Form1 : Form
 	{
+		private Dict dictionary = new Dict();
+		private string[] words;
+
 		public Form1()
 		{
 			InitializeComponent();
@@ -20,7 +23,6 @@ namespace TestDialogs
 
 		private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
 		{
-
 		}
 
 		private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
@@ -35,7 +37,6 @@ namespace TestDialogs
 				else
 					richTextBox1.LoadFile(fd.FileName, RichTextBoxStreamType.RichText);
 			}
-
 		}
 
 		private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
@@ -87,7 +88,6 @@ namespace TestDialogs
 			{
 				richTextBox1.SelectionColor = d.Color;
 			}
-
 		}
 
 		private void toolStripButton2_Click(object sender, EventArgs e)
@@ -112,6 +112,138 @@ namespace TestDialogs
 		private void создатьToolStripButton_Click(object sender, EventArgs e)
 		{
 			richTextBox1.Text = "";
+		}
+
+		private void toolStripButton3_Click(object sender, EventArgs e)
+		{
+			richTextBox1.SelectionAlignment = HorizontalAlignment.Left;
+		}
+
+		private void toolStripButton4_Click(object sender, EventArgs e)
+		{
+			richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+		}
+
+		private void toolStripButton5_Click(object sender, EventArgs e)
+		{
+			richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+		}
+
+		private void создатьToolStripMenuItem_MouseEnter(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Создать новый документ";
+		}
+
+		private void выходToolStripMenuItem_MouseEnter(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Выход из программы";
+		}
+
+		private void создатьToolStripButton_MouseEnter(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Создать новый документ";
+		}
+
+		private void toolStripButton1_MouseEnter(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Изменить цвет текста";
+		}
+
+		private void toolStripButton2_MouseEnter(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Работа с шрифтами";
+		}
+
+		private void toolStripButton3_MouseEnter(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Выровнять по левому краю";
+		}
+
+		private void toolStripButton4_MouseEnter(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Выровнять по центру";
+		}
+
+		private void toolStripButton5_MouseEnter(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Выровнять по правому краю";
+		}
+
+		private void открытьToolStripButton_MouseEnter_1(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Открыть документ";
+		}
+
+		private void сохранитьToolStripButton_MouseEnter_1(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Сохранить документ";
+		}
+
+		private void открытьToolStripMenuItem_MouseEnter_1(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Открыть документ";
+		}
+
+		private void сохранитьToolStripMenuItem_MouseEnter(object sender, EventArgs e)
+		{
+			toolStripStatusLabel1.Text = "Сохранить документ";
+		}
+
+		private void перевестиСловоНаАнглийскийToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			richTextBox1.SelectedText = dictionary.dicti[richTextBox1.SelectedText];
+		}
+
+		private void перевестиСтрокуНаАнглийскийToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			words = richTextBox1.SelectedText.Split(' ');
+			string final = "";
+			int i = 0;
+			foreach (string c in words)
+			{
+				Console.WriteLine(words[i]);
+				try
+				{
+					words[i] = dictionary.dicti[words[i]];
+				}
+				catch (System.Collections.Generic.KeyNotFoundException)
+				{
+				}
+				final = string.Join(" ", words);
+				i++;
+			}
+			richTextBox1.SelectedText = final;
+		}
+	}
+
+	public class Dict
+	{
+		private string key = "";
+		private string value = "";
+		public Dictionary<string, string> dicti = new Dictionary<string, string>();
+
+		public Dict()
+		{
+			try
+			{
+				using (StreamReader fs = new StreamReader("dictionary.txt"))
+				{
+					while (true)
+					{
+						key = fs.ReadLine();
+						Console.WriteLine(key);
+						Console.WriteLine(value);
+						value = fs.ReadLine();
+						if (key == null || value == null)
+							break;
+						dicti.Add(key, value);
+					}
+				}
+			}
+			catch (System.IO.FileNotFoundException e)
+			{
+				MessageBox.Show("Dictionary not found");
+			}
 		}
 	}
 }
